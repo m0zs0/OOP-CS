9ec8d15863663cdcd770b06f20d727996802bf38
mozso <126395568+m0zs0@users.noreply.github.com>
2024-12-29 20:08:13 +0100

    Add files via upload

---
+++ NASA/Keres.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace NASA
+{
+    internal class Keres
+    {
+        public Keres(string s)
+        {
+            string [] seged = s.Split('*');
+            Cim = seged[0];
+            DatumIdo = DateTime.ParseExact(seged[1], "dd/MMM/yyyy:HH:mm:ss", CultureInfo.InvariantCulture); 
+            Get = seged[2];
+            HttpKod = seged[3].Split()[0];
+            Meret = seged[3].Split()[1];
+        }
+
+        public string Cim { get; set; }
+        public DateTime DatumIdo { get; set; }
+        public string Get { get; set; }
+        public string HttpKod { get; set; }
+        public string Meret { get; set; }
+
+        public int ByteMeret 
+        {
+            get
+            {
+                if (Meret == "-") return 0;
+                return int.Parse(Meret);
+            }
+        }
+
+        public bool Domain 
+        {
+            get
+            {
+                if (Char.IsLetter(Cim[Cim.Length - 1])) return true;
+                else return false;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"címe: {Cim} ideje: {DatumIdo} állapotkód: {HttpKod} méret: {Meret}";
+        }
+
+    }
+}
---
+++ NASA/Nasa.cs
@@ -0,0 +1,88 @@
+﻿using NASA;
+using System.Text;
+
+static List<Keres> Beolvas() { 
+    List<Keres> k = new List<Keres>();
+    try
+    {
+        using (StreamReader sr = new StreamReader("NASAlog.txt", Encoding.UTF8))
+        {
+            string sor;
+            while ((sor = sr.ReadLine()) != null)
+            {
+                k.Add(new Keres(sor));
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Hiba: {ex.Message}");
+        Console.ReadKey();
+        Environment.Exit(1);
+    }
+    return k;
+}
+
+static int ValaszokOsszesMerete(List<Keres> k) {
+    int osszMeret = k.Sum(k => k.ByteMeret);
+    /*int osszMeret = 0;
+    foreach (Keres keres in k) {
+        osszMeret += keres.ByteMeret;
+    }*/
+    return osszMeret;
+}
+
+static int DomainekSzama(List<Keres> k) {
+    int db = k.Count(k => k.Domain);
+    /*int db = 0;
+    foreach (Keres keres in k) {
+        if (keres.Domain) {
+            db++;
+        }
+    }*/
+    return db;
+}
+
+static void Statisztika(List<Keres> k) {
+    IEnumerable<IGrouping<string, Keres>> csoportok = k.GroupBy(k => k.HttpKod);
+    foreach (IGrouping<string, Keres> csoport in csoportok) { 
+        Console.WriteLine($"\t{csoport.Key}: {csoport.Count()} db");
+    }
+
+    /*Dictionary<string, int> stat = new Dictionary<string, int>();
+    foreach (Keres keres in k) {
+        if (stat.ContainsKey(keres.HttpKod)) {
+            stat[keres.HttpKod]++;
+        }
+        else {
+            stat.Add(keres.HttpKod, 1);
+        }
+    }
+    foreach (KeyValuePair<string, int> item in stat) {
+        Console.WriteLine($"\t{item.Key}: {item.Value} db");
+    }*/
+}
+
+static Keres ElsoDomainNelkuliKeres(List<Keres> k) {
+    /*Keres keresett = null;
+    foreach (Keres keres in k)
+    {
+        if (!keres.Domain)
+        {
+            keresett = keres;
+            break;
+        }
+    }
+    return keresett;*/
+
+    return k.FirstOrDefault(k => !k.Domain);
+}
+
+List<Keres> keresek = Beolvas();
+Console.WriteLine($"5. feladat: Kérések száma: {keresek.Count}");
+Console.WriteLine($"6. feladat: Válaszok összes mérete: {ValaszokOsszesMerete(keresek)} byte");
+Console.WriteLine($"8. Feladat: Domain-es kérések: {(double)DomainekSzama(keresek) / keresek.Count:P}");
+Console.WriteLine("9. feladat: Statisztika");
+Statisztika(keresek);
+Keres elsoDomainNelkuli = ElsoDomainNelkuliKeres(keresek);
+Console.WriteLine($"10. feladat: Az első Domain nélküli kérés adatai: \n{(elsoDomainNelkuli == null?"Nincs ilyen": elsoDomainNelkuli)}");
